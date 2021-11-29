95a04f57ee9ee455f883e4455a96ebf2a67c8c43
haikallf <haikallzrd@gmail.com>
2021-11-29 20:00:22 +0700

    formatting

--- transaction.py
+++ transaction.py
@@ -1,79 +1,76 @@
 from queue import Queue
-import time
 
-def setExclusiveLock(Locks, a, idx, sequence):
-    if (a.getAction(idx) == 'W' or (a.getAction(idx) == 'R')):
-        sequence.enqueue(('XL', a.getTransaction(idx), a.getVar(idx)))
-        Locks.enqueue((a.getVar(idx), a.getTransaction(idx)))
-        print('exclusive locks of', a.getVar(idx), "in transaction", a.getTransaction(idx))
-        print('XL'+a.getTransaction(idx)+'('+a.getVar(idx)+')')
+def setExclusiveLock(Locks, soal, idx, solusi):
+    if (soal.getAction(idx) == 'W' or (soal.getAction(idx) == 'R')):
+        solusi.enqueue(('XL', soal.getTransaction(idx), soal.getVar(idx)))
+        Locks.enqueue((soal.getVar(idx), soal.getTransaction(idx)))
+        print('XL'+soal.getTransaction(idx)+'('+soal.getVar(idx)+')'+"; ", end="")
 
-def setUnlock(Locks, a, idx, sequence):
-    if (a.getAction(idx) == 'C'):
+def setUnlock(Locks, soal, idx, solusi):
+    if (soal.getAction(idx) == 'C'):
         var = []
         for elmt in Locks.items:
-            if(elmt[1] ==  a.getTransaction(idx)):
+            if(elmt[1] ==  soal.getTransaction(idx)):
                 var.append(elmt[0])
         for variable in var:
-            sequence.enqueue(('UL', a.getTransaction(idx), variable))
-            lockidx = Locks.findIdx((variable, a.getTransaction(idx)))
+            solusi.enqueue(('UL', soal.getTransaction(idx), variable))
+            lockidx = Locks.findIdx((variable, soal.getTransaction(idx)))
             Locks.dequeueAtIdx(lockidx)
-            print('UL'+a.getTransaction(idx)+'('+variable+')')
+            print('UL'+soal.getTransaction(idx)+'('+variable+')'+"; ", end="")
 
-def isLock(Locks, a, idx):
+def isLock(Locks, soal, idx):
     var = []
-    trans = []
     for elmt in Locks.items:
         var.append(elmt[0])
-    if (a.getVar(idx) in var):
+    if (soal.getVar(idx) in var):
         return True
 
-def isAvailable(Locks, a, idx):
-    if(a.getAction(idx) != 'C'):
+def isAvailable(Locks, soal, idx):
+    if(soal.getAction(idx) != 'C'):
         trans = []
         for elmt in Locks.items:
-            if(elmt[0] == a.getVar(idx)):
+            if(elmt[0] == soal.getVar(idx)):
                 trans.append(elmt[1])
-        if (isLock(Locks, a, idx) and a.getTransaction(idx) not in trans):
+        if (isLock(Locks, soal, idx) and soal.getTransaction(idx) not in trans):
             return False
     return True
         
-def isTransactionAvailable(Locks, a, idx):
-    if (isAvailable(Locks, a, idx)):
+def isTransactionAvailable(Locks, soal, idx):
+    if (isAvailable(Locks, soal, idx)):
         for i in range(idx):
-            if(a.getTransaction(i) == a.getTransaction(idx)):
+            if(soal.getTransaction(i) == soal.getTransaction(idx)):
                 return False
         return True
     return False
 
-a = Queue()
-text_file = open("a.txt", "r")
+soal = Queue()
+text_file = open("input.txt", "r")
 lines = text_file.read().split(',')
 for elmt in lines:
     if(elmt[0] == 'C'):
-        a.enqueue((elmt[0], elmt[1]))
+        soal.enqueue((elmt[0], elmt[1]))
     else: 
-        a.enqueue((elmt[0], elmt[1], elmt[2]))
-print("input file: ")
-a.printQueue()
+        soal.enqueue((elmt[0], elmt[1], elmt[2]))
 
+print("Soal: ", end="")
+soal.printQueue()
 
+print("Solusi: ", end="")
 
-sequence = Queue()
+solusi = Queue()
 Locks = Queue()
 i = 0
-while (a.size() > 0):
-    if(isTransactionAvailable(Locks, a, i)):
-        setExclusiveLock(Locks, a, i, sequence)
-        setUnlock(Locks, a, i, sequence)
-        sequence.enqueue(a.items[i])
-        if(a.getAction(i) != 'C'):
-            print(a.getAction(i)+a.getTransaction(i)+'('+a.getVar(i)+')')
+while (soal.size() > 0):
+    if(isTransactionAvailable(Locks, soal, i)):
+        setExclusiveLock(Locks, soal, i, solusi)
+        setUnlock(Locks, soal, i, solusi)
+        solusi.enqueue(soal.items[i])
+        if(soal.getAction(i) != 'C'):
+            print(soal.getAction(i)+soal.getTransaction(i)+'('+soal.getVar(i)+')'+"; ", end="")
         else: 
-            print(a.getAction(i)+a.getTransaction(i))
-        a.dequeueAtIdx(i)
+            print(soal.getAction(i)+soal.getTransaction(i)+ "; ", end="")
+        soal.dequeueAtIdx(i)
     else:
         i += 1
-    if (i == a.size()):
+    if (i == soal.size()):
         i = 0
-    # time.sleep(8)
